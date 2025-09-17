0b06bcad165fe1a9574bbe383fa55b9353169036
Md. Aseer Ishraq Tasin <126379641+tasin-ishraq@users.noreply.github.com>
2025-09-17 12:18:18 +0600

    Add files via upload

---
+++ Celcious to Fahrenheit.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+float c,f;
+
+printf("Enter the Celcious Value:\n");
+scanf("%f",&c);
+
+f=32+(1.8*c);
+
+printf("The Fahrenheit Value is=%f",f);
+
+return 0;
+
+}
+
